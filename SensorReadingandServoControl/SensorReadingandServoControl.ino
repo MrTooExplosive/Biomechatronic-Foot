045cf4caab95fdc0b0f58c51166990b8bf3aa918
Makarius Salib <misalib@cpp.edu>
2022-03-31 13:07:02 -0700

    Allows for ability to control servos
    Adds macro variables for the first PWM pin and the number of servos
     - Allows for easy changing of the sketch due to different boards
     - Allows for easy editing if the number of servos changes
    Attaches pins starting from the first PWM pin

--- SensorReadingandServoControl/SensorReadingandServoControl.ino
+++ SensorReadingandServoControl/SensorReadingandServoControl.ino
@@ -1,9 +1,18 @@
+#include <Servo.h>
+
+#define PWM_START 2 // Initial pin number for PWM pins
+#define SERVO_COUNT 1
+
+Servo servos[SERVO_COUNT];
+
 void setup()
 {
-  
+  // Attach the signal pin for each servo
+  // Servo pins start at pin2
+  for (int i = 0; i < SERVO_COUNT; i++)
+    servos[i].attach(i + PWM_START);
 }
 
 void loop()
 {
-  
 }
