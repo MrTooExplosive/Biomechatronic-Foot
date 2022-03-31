d2d97a6de6d5b9e3db8afc0b72a1c4a42e2382e7
Makarius Salib <misalib@cpp.edu>
2022-03-31 14:17:27 -0700

    Allows for reading pressure transducer values

--- SensorReadingandServoControl/SensorReadingandServoControl.ino
+++ SensorReadingandServoControl/SensorReadingandServoControl.ino
@@ -3,16 +3,23 @@
 #define PWM_START 2 // Initial pin number for PWM pins
 #define SERVO_COUNT 0 // Currently zero for testing
 #define ANALOG_START 0 // Initial pin number for analog pins
-#define POT_COUNT 1
+#define POT_COUNT 0 // Currently zero for testing
+#define PRESSURE_COUNT 1 
 
 Servo servos[SERVO_COUNT];
 int initalPotValues[POT_COUNT];
+int initialPressureVals[PRESSURE_COUNT];
 
 
 void setup()
 {
   Serial.begin(9600); // Set BAUD rate
 
+  // Get the initial pressures of each pressure transducer
+  // Will be useful later
+  for (int i = 0; i < PRESSURE_COUNT; i++)
+    initialPressureVals[i] = analogRead(i + ANALOG_START + POT_COUT); // Pressure tranducers will be attached after potentiometers
+    
   // Get the initial position of each potentiometer
   // Used for the finding the relative positions
   for (int i = 0; i < POT_COUNT; i++)
